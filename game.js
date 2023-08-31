67526592c01c4d0366022e167309b3a132c23f24
Dávid <davhaj.hajko@gmail.com>
2023-08-31 19:36:10 +0200

    Prvé premeny

---
+++ game.js
@@ -0,0 +1,17 @@
+console.log("Piskvorky");
+
+//Premeny
+const p1 = "X", p2 = "O";
+let moves = 0;
+let moveComputer = false;
+let isWinner = false;
+
+let score = {
+    player: 0,
+    ai: 0
+};
+
+const scoreText = document.getElementById("score");
+let buttons = document.getElementsByTagName("button");
+
+
